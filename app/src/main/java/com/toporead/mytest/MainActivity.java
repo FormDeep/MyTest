489e6d95483aaa3e407ddbec57832648c44161c0
FormDeep <formdeep@gmail.com>
2024-01-15 19:45:27 +0800

    2024/1/15v1

--- app/src/main/java/com/toporead/mytest/MainActivity.java
+++ app/src/main/java/com/toporead/mytest/MainActivity.java
@@ -1,14 +1,30 @@
 package com.toporead.mytest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> myData = Arrays.asList("text1","text2","text3");
+        recyclerView =findViewById(R.id.myrecyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter =new MyAdapter(myData);
+        recyclerView.setAdapter(myAdapter);
     }
 }
